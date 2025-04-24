2d77f6be52c0e696295bffe91ee09495d5176902
ajchilts <40767309+ajchilts@users.noreply.github.com>
2025-04-24 16:36:35 +0100

    ADR-2008 | Add retry aling with circuit breaker

--- app/uk/gov/hmrc/alcoholdutyaccount/connectors/SubscriptionSummaryConnector.scala
+++ app/uk/gov/hmrc/alcoholdutyaccount/connectors/SubscriptionSummaryConnector.scala
@@ -16,8 +16,10 @@
 
 package uk.gov.hmrc.alcoholdutyaccount.connectors
 
+import org.apache.pekko.pattern.retry
+import org.apache.pekko.actor.{ActorSystem, Scheduler}
 import play.api.Logging
-import play.api.http.Status.{BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND}
+import play.api.http.Status.{BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND, OK}
 import uk.gov.hmrc.alcoholdutyaccount.config.{AppConfig, SubscriptionCircuitBreakerProvider}
 import uk.gov.hmrc.alcoholdutyaccount.connectors.helpers.HIPHeaders
 import uk.gov.hmrc.alcoholdutyaccount.models.hods.{SubscriptionSummary, SubscriptionSummarySuccess}
@@ -29,61 +31,64 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
+// TODO - Put debug statements back in
+// TODO - Ensure retry mechanism set to 1 for spec tests
+// TODO - Add one spec test that sets the retry > 1 and checks the API is called multiple times
+
 class SubscriptionSummaryConnector @Inject() (
   config: AppConfig,
   headers: HIPHeaders,
   subscriptionCircuitBreakerProvider: SubscriptionCircuitBreakerProvider,
+  implicit val system: ActorSystem,
   implicit val httpClient: HttpClientV2
 )(implicit ec: ExecutionContext)
     extends HttpReadsInstances
     with Logging {
 
+  implicit val scheduler: Scheduler = system.scheduler
+
   def getSubscriptionSummary(
     appaId: String
   )(implicit hc: HeaderCarrier): Future[Either[ErrorResponse, SubscriptionSummary]] =
+    retry(
+      () => call(appaId),
+      attempts = config.retryAttempts,
+      delay = config.retryAttemptsDelay
+    ).map { response =>
+      Right(response)
+    }.recoverWith {
+      case badRequest: BadRequestException     => Future.successful(Left(ErrorResponse(BAD_REQUEST, badRequest.getMessage)))
+      case notFound: NotFoundException         => Future.successful(Left(ErrorResponse(NOT_FOUND, notFound.getMessage)))
+      case illegalState: IllegalStateException =>
+        Future.successful(Left(ErrorResponse(INTERNAL_SERVER_ERROR, illegalState.getMessage)))
+      case _                                   => Future.successful(Left(ErrorResponse(INTERNAL_SERVER_ERROR, "An error occurred")))
+    }
+
+  def call(
+    appaId: String
+  )(implicit hc: HeaderCarrier): Future[SubscriptionSummary] =
     subscriptionCircuitBreakerProvider.get().withCircuitBreaker {
-      logger.info(s"Fetching subscription summary for appaId $appaId")
       httpClient
         .get(url"${config.getSubscriptionUrl(appaId)}")
         .setHeader(headers.subscriptionHeaders(): _*)
-        .execute[Either[UpstreamErrorResponse, HttpResponse]]
-        .map {
-          case Right(response) =>
-            Try {
-              response.json
-                .as[SubscriptionSummarySuccess]
-            } match {
-              case Success(doc)       =>
-                logger.info(s"Retrieved subscription summary success for appaId $appaId")
-                Right(doc.success)
-              case Failure(exception) =>
-                logger.warn(s"Unable to parse subscription summary success for appaId $appaId", exception)
-                Left(ErrorResponse(INTERNAL_SERVER_ERROR, "Unable to parse subscription summary success"))
-            }
-          case Left(error)     => Left(processError(error, appaId))
+        .execute[HttpResponse]
+        .flatMap { response =>
+          response.status match {
+            case OK          =>
+              Try {
+                response.json
+                  .as[SubscriptionSummarySuccess]
+              } match {
+                case Success(doc) =>
+                  Future.successful(doc.success)
+                case Failure(_)   =>
+                  Future
+                    .failed(new IllegalStateException(s"Unable to parse subscription summary success"))
+              }
+            case BAD_REQUEST => Future.failed(new BadRequestException("Bad request"))
+            case NOT_FOUND   => Future.failed(new NotFoundException("Subscription summary not found"))
+            case _           => Future.failed(new InternalServerException("An error occurred"))
+          }
         }
-        .recoverWith { case e: Exception =>
-          logger.warn(
-            s"An exception was returned while trying to fetch subscription summary appaId $appaId",
-            e
-          )
-          Future.successful(Left(ErrorResponse(INTERNAL_SERVER_ERROR, e.getMessage)))
-        }
-    }
-
-  private def processError(error: UpstreamErrorResponse, appaId: String): ErrorResponse =
-    error.statusCode match {
-      case BAD_REQUEST =>
-        logger.info(s"Bad request sent to get subscription for appaId $appaId", error)
-        ErrorResponse(BAD_REQUEST, "Bad request")
-      case NOT_FOUND   =>
-        logger.info(s"No subscription summary found for appaId $appaId")
-        ErrorResponse(NOT_FOUND, "Subscription summary not found")
-      case _           =>
-        logger.warn(
-          s"An error was returned while trying to fetch subscription summary appaId $appaId",
-          error
-        )
-        ErrorResponse(INTERNAL_SERVER_ERROR, "An error occurred")
     }
 }
